82b420962d62c81b2379b9aedc2530363e20fa3c
Sanjai T S <sanjaits@Sanjais-MacBook-Air.local>
2022-05-22 01:01:40 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const API_KEY = "1032c5bb249668ca063747a1198441d9";
+
+let date = document.getElementById("date");
+let time = document.getElementById("time");
+let cityName = document.getElementById("cityName");
+let searchCityName = document.getElementById("searchCityName");
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function convertEpoch(value) {
+    var utcSeconds = value * 1000;
+    var d = new Date(utcSeconds);
+    console.log(d.toLocaleString())
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let hours = d.getHours();
+    let mins = d.getMinutes();
+    let secs = d.getSeconds();
+
+    return {
+        currentDate: `${date} ${month} ${year}`,
+        currentTime: `${hours}:${mins}:${secs}`
+        }; 
+}
+
+searchCityName.addEventListener('click', getWeather)
+
+function getWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    dateTime = convertEpoch(data.dt);
+    date.innerText = dateTime.currentDate;
+    time.innerText = dateTime.currentTime;
+});
+}
